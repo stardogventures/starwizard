5b0620ab23ffbab9339fcc4fc9145f4134911ad2
Ian White <ibwhite@gmail.com>
2016-11-09 04:50:11 -0500

    removing the email-template related stuff, moving that to its own module, since it has nothing to do with dropwizard

--- src/main/java/io/stardog/email/interfaces/EmailRawSender.java
+++
@@ -1,9 +0,0 @@
-package io.stardog.email.interfaces;
-
-/**
- * An EmailRawSender is capable of sending raw emails.
- */
-public interface EmailRawSender {
-    String sendEmail(String toEmail, String toName, String fromEmail, String fromName,
-                     String subject, String contentHtml, String contentText);
-}
