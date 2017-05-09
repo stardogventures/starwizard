e1d1771e533cdeef744e11ce88e070554f0613c0
Ian White <ibwhite@gmail.com>
2017-05-09 13:56:35 -0400

    setting swagger names for AccessTokenResponse

--- starwizard-core/src/main/java/io/stardog/starwizard/data/response/AccessTokenResponse.java
+++ starwizard-core/src/main/java/io/stardog/starwizard/data/response/AccessTokenResponse.java
@@ -9,23 +9,23 @@ import java.util.Optional;
 @AutoValue
 public abstract class AccessTokenResponse {
     @JsonProperty("access_token")
-    @ApiModelProperty(value = "access token value", example = "2YotnFZFEjr1zCsicMWsdv")
+    @ApiModelProperty(name = "access_token", value = "access token value", example = "2YotnFZFEjr1zCsicMWsdv")
     public abstract String getAccessToken();
 
     @JsonProperty("token_type")
-    @ApiModelProperty(value = "access token type", example = "bearer")
+    @ApiModelProperty(name = "token_type", value = "access token type", example = "bearer")
     public abstract String getTokenType();
 
     @JsonProperty("expires_in")
-    @ApiModelProperty(value = "expiration time of token, in seconds", example = "86400")
+    @ApiModelProperty(name = "expires_in", value = "expiration time of token, in seconds", example = "86400")
     public abstract int getExpiresIn();
 
     @JsonProperty("refresh_token")
-    @ApiModelProperty(value = "refresh token value", example = "tGzv3JOkF0XG5Qx2TlKWIA")
+    @ApiModelProperty(name = "refresh_token", value = "refresh token value", example = "tGzv3JOkF0XG5Qx2TlKWIA")
     public abstract Optional<String> getRefreshToken();
 
     @JsonProperty("scope")
-    @ApiModelProperty(value = "scope of the access token", example = "admin")
+    @ApiModelProperty(name = "scope", value = "scope of the access token", example = "admin")
     public abstract Optional<String> getScope();
 
     public static AccessTokenResponse of(String accessToken, String tokenType, int expiresIn) {
