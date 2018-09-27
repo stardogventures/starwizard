6977e3fee5ffeeb8a1c19d125ebffcc439fc12c1
Ian White <ibwhite@gmail.com>
2018-09-27 11:03:37 -0400

    fixing AsyncService constructor to allow caller to specify thread pool rules

--- starwizard-services/src/main/java/io/stardog/starwizard/services/common/AsyncService.java
+++ starwizard-services/src/main/java/io/stardog/starwizard/services/common/AsyncService.java
@@ -2,19 +2,24 @@ package io.stardog.starwizard.services.common;
 
 import io.dropwizard.lifecycle.Managed;
 
+import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 /**
  * A shared service for executing lightweight asynchronous processes and managing their thread pool using Dropwizard
- * lifecycle.
+ * lifecycle. Essentially just a wrapper around ExecutorService that implements the Dropwizard lifecycle.
  */
 @Singleton
 public class AsyncService implements Managed {
-    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
+    private final ExecutorService executorService;
+
+    @Inject
+    public AsyncService(ExecutorService executorService) {
+        this.executorService = executorService;
+    }
 
     @Override
     public void start() throws Exception {
